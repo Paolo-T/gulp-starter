fb4d757f64f836f9895ba275d35b55233db25ff8
Paolo-T <paolo.todde@yahoo.com>
2019-03-17 19:33:14 +0000

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const uglify = require('gulp-uglify');
+const imgmin = require('gulp-imagemin');
 const browserSync = require('browser-sync').create;
 
 // Compile sass
@@ -9,7 +11,11 @@ function styles() {
 			// 1. Where is my my sass file
 			.src('./src/assets/sass/main.sass')
 			// 2. pass the file through the sass compiler
-			.pipe(sass())
+			.pipe(
+				sass({
+					ouputStyle: 'expanded'
+				}).on('error', sass.logError)
+			)
 			// 3. Where do I save the compiled css?
 			.pipe(gulp.dest('./dist/assets/css/'))
 			// 4. Stream changes to all browsers
@@ -20,11 +26,13 @@ function styles() {
 function watch() {
 	browserSync.init({
 		server: {
-			baseDir: 'dist'
+			baseDir: './src/'
 		}
 	});
-	gulp.watch('./src/assets/sass/main.sass, styles');
-	gulp.watch('./*.html').on('change, browserSync.reload');
+	gulp.watch('./src/assets/sass/main.sass', styles);
+	gulp.watch('./*.html').on('change', browserSync.reload);
+	gulp.watch('./src/assets/**/*.js').on('change', browserSync.reload);
 }
 
 exports.styles = styles;
+exports.watch = watch;
